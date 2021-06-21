20c4af67d7808e94e71f3a6739aecfe85abe579d
醉翁之意 <Judy_u@163.com>
2021-06-21 14:27:01 +0800

    公开函数、属性

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,13 +1,11 @@
 import SwiftyJSON
 
 public struct MyLibrary {
-    var text = "Hello, World!"
+    public var text = "Hello, World!"
 
-    public init() {
-        
-    }
+    public init() { }
     
-    func test() -> String {
+    public func test() -> String {
         var json = JSON()
         json["userName"] = JSON("王仁洁")
         return json["userName"].stringValue
