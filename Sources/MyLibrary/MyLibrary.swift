f66c47d7bc705fb34bff469d9965c1bef1318ce7
醉翁之意 <Judy_u@163.com>
2021-06-21 14:23:00 +0800

    依赖SwiftyJSON

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,3 +1,11 @@
-struct MyLibrary {
+import SwiftyJSON
+
+public struct MyLibrary {
     var text = "Hello, World!"
+    
+    func test() -> String {
+        var json = JSON()
+        json["userName"] = JSON("王仁洁")
+        return json["userName"].stringValue
+    }
 }
